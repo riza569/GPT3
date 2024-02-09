374403129f43b27e37ea51d61bb94329310ac0f6
riza569 <rizamuhammed569@gmail.com>
2024-02-10 02:09:00 +0530

    fghjk

--- src/App.js
+++ src/App.js
@@ -3,13 +3,7 @@ import "./app.css";
 import Navbar from "./components/Navbar";
 import Header from "./container/header";
 import Brand from "./components/brand";
-import WhatGPT3 from "./container/whatGPT3";
-import Features from "./container/features";
-import Possibility from "./container/possibility";
-import Cta from "./components/Cta";
-import Blog from "./container/blog";
-import Footer from "./container/Footer";
-// Corrected Import Statement
+import WhatGP
 const App = () => {
   return (
     <div className="App">
